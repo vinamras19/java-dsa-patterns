79c8d949e47b0d3e8a015957f05a15cdeea7998d
Vinamra Singh <vinamrasingh19@gmail.com>
2026-02-07 12:20:29 -0500

    LC15

---
+++ 15_3Sum.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    while (nums[l] == nums[l-1] && l < r) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
