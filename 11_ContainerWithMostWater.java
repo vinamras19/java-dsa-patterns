67c7929f0e4ba1489a2717553126baaefdfaad43
Vinamra Singh <vinamrasingh19@gmail.com>
2026-02-09 00:54:31 -0500

    LC11

---
+++ 11_ContainerWithMostWater.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int res = 0;
+
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            res = Math.max(res, area);
+
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+}
