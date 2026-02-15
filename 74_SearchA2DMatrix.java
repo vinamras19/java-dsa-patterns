5b6bb7447948ccfe2ed591fc8507cac313e36400
Vinamra Singh <vinamrasingh19@gmail.com>
2026-02-14 23:09:14 -0500

    LC74

---
+++ 74_SearchA2DMatrix.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int row = mid / cols;
+            int col = mid % cols;
+            int midVal = matrix[row][col];
+
+            if (midVal == target) {
+                return true;
+            }
+            else if (midVal < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
