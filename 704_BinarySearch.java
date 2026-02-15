5df031329ff9ed96416ee197ed493a7c9d7d8c6f
Vinamra Singh <vinamrasingh19@gmail.com>
2026-02-14 23:09:04 -0500

    LC704

---
+++ 704_BinarySearch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1; // search right half
+            }
+            else {
+                right = mid - 1; // search left half
+            }
+        }
+
+        return -1; // not found
+    }
+}
