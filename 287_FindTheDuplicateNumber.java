77873701ada45b56a73d435153ef8c50d79301c2
Vinamra Singh <vinamrasingh19@gmail.com>
2026-02-22 22:48:08 -0500

    LC287

---
+++ 287_FindTheDuplicateNumber.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        // Floyd cycle detection
+        int slow = nums[0];
+        int fast = nums[0];
+        // find intersection point
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        // find cycle start
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
