33e84f58575f4082ae9fea599df426fdb04ec584
Vinamra Singh <vinamrasingh19@gmail.com>
2026-03-12 16:00:55 -0400

    LC98

---
+++ 98_ValidateBinarySearchTree.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
+    }
+    private boolean validate(TreeNode node, Integer min, Integer max) {
+        if (node == null) return true;
+        if (min != null && node.val <= min) return false;
+        if (max != null && node.val >= max) return false;
+        return validate(node.left, min, node.val) &&
+                validate(node.right, node.val, max);
+    }
+}
