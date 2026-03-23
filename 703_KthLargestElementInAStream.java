0626b50e75a0fa5002f667c061cac7ec62fcbe9c
Vinamra Singh <vinamrasingh19@gmail.com>
2026-03-23 11:49:12 -0400

    LC703

---
+++ 703_KthLargestElementInAStream.java
@@ -0,0 +1,14 @@
+class KthLargest {
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for (int n : nums) add(n);
+    }
+    public int add(int val) {
+        minHeap.offer(val);
+        if (minHeap.size() > k) minHeap.poll();
+        return minHeap.peek();
+    }
+}
