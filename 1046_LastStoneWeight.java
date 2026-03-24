2e7b784b97416741d2efd903fb520b025519cebf
Vinamra Singh <vinamrasingh19@gmail.com>
2026-03-24 14:10:16 -0400

    LC1046

---
+++ 1046_LastStoneWeight.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int s : stones) maxHeap.offer(s);
+        while (maxHeap.size() > 1) {
+            int a = maxHeap.poll();
+            int b = maxHeap.poll();
+            if (a != b) maxHeap.offer(a - b);
+        }
+        return maxHeap.isEmpty() ? 0 : maxHeap.peek();
+    }
+}
