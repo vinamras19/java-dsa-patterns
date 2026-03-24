0a885e0737f7d99cf690ba3eedf879ae7cce0a88
Vinamra Singh <vinamrasingh19@gmail.com>
2026-03-24 14:09:37 -0400

    LC973

---
+++ 973_KClosestPointsToOrigin.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+                (a, b) -> (b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1])
+        );
+        for (int[] p : points) {
+            maxHeap.offer(p);
+            if (maxHeap.size() > k) maxHeap.poll();
+        }
+        return maxHeap.toArray(new int[k][]);
+    }
+}
