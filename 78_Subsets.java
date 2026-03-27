83b4af7092932f6d38bff53f2aa01feddaac8c86
Vinamra Singh <vinamrasingh19@gmail.com>
2026-03-27 13:22:01 -0400

    LC78

---
+++ 78_Subsets.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, List<Integer> cur, List<List<Integer>> res) {
+        res.add(new ArrayList<>(cur));
+        for (int i = start; i < nums.length; i++) {
+            cur.add(nums[i]);
+            backtrack(nums, i + 1, cur, res);
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
