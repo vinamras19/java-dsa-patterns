5b615472d23a02116a367a7f6456eca583efdf8c
Vinamra Singh <vinamrasingh19@gmail.com>
2026-03-28 13:34:28 -0400

    LC90

---
+++ 90_SubsetsII.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, List<Integer> cur, List<List<Integer>> res) {
+        res.add(new ArrayList<>(cur));
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue;
+            cur.add(nums[i]);
+            backtrack(nums, i + 1, cur, res);
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
