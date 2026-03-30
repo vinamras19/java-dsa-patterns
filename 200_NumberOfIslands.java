d05ffc38f2eaa923bcc65c979bfbbdce9d6eb6ee
Vinamra Singh <vinamrasingh19@gmail.com>
2026-03-30 17:28:02 -0400

    LC200

---
+++ 200_NumberOfIslands.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for (int r = 0; r < grid.length; r++)
+            for (int c = 0; c < grid[0].length; c++)
+                if (grid[r][c] == '1') { dfs(grid, r, c); count++; }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] != '1') return;
+        grid[r][c] = '0';
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
+    }
+}
