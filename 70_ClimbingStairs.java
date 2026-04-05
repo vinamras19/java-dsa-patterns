5dbc4989b04bd2650fead38aa4976eede6a07c67
Vinamra Singh <vinamrasingh19@gmail.com>
2026-04-05 17:54:37 -0400

    LC70

---
+++ 70_ClimbingStairs.java
@@ -0,0 +1,14 @@
+class Solution
+{
+    public int climbStairs(int n)
+    {
+        int a = 1, b = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            int tmp = a + b;
+            a = b;
+            b = tmp;
+        }
+        return b;
+    }
+}
