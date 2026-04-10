a4edae3bc804ad30a2653440feca5a489a336171
Vinamra Singh <vinamrasingh19@gmail.com>
2026-04-10 00:41:47 -0400

    LC309

---
+++ 309_StockwithCooldown.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int hold = -prices[0], sold = 0, rest = 0;
+        for (int i = 1; i < prices.length; i++) {
+            int prevHold = hold, prevSold = sold, prevRest = rest;
+            hold = Math.max(prevHold, prevRest - prices[i]);
+            sold = prevHold + prices[i];
+            rest = Math.max(prevRest, prevSold);
+        }
+        return Math.max(sold, rest);
+    }
+}
